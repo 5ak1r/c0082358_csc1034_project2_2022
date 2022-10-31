a133e7698c945ba427c6a1fdd5865894fab76b9b
5ak1r <s.azimkar1@newcastle.ac.uk>
2022-10-31 10:23:03 +0000

    Added checks for __init__ inputs Moved start GUI into main

--- client.py
+++ client.py
@@ -1,8 +1,33 @@
+from datetime import datetime as dt
+
 class Client:
 
     # Creates a class
-    def __init__(self, name):
-        if not isinstance(name, str):
-            raise TypeError("That is not a name")
+    def __init__(self, fname, lname, title, pp, dob, occupation, bal, overlim):
+        if not isinstance(fname, str):
+            raise TypeError("That is not a valid first name.")
+        if not isinstance(lname, str):
+            raise TypeError("That is not a valid last name.")
+        if not isinstance(title, str):
+            raise TypeError("That is not a valid title.")
+        if not isinstance(pp, str):
+            raise TypeError("That is not a valid set of pronouns.")
+        if not isinstance(dob, str):
+            raise TypeError("That is not a valid date of birth.")
+            dob_check = dob.split("/")
+            if dob_check[1].int() in [9,4,5,6,11] and (dob_check[0].int() > 30 or (dob_check[2].int()>dt.now()[0:4].int() or dob_check[2].int()<(dt.now()[0:4].int()-100))):
+                raise ValueError("That is not a valid date of the form DD/MM/YYYY")
+            elif dob_check[1].int() == 2 and dob_check[0].int() > 28:
+                if dob_check[2] % 4 != 0 and (dob_check[0].int() > 29 or (dob_check[2].int()>dt.now()[0:4].int() or dob_check[2].int()<(dt.now()[0:4].int()-100))):
+                    raise ValueError("That is not a valid date of the form DD/MM/YYYY")
+            elif dob_check[0].int() > 31 or (dob_check[2].int()>dt.now()[0:4].int() or dob_check[2].int()<(dt.now()[0:4].int()-100)):
+                raise ValueError("That is not a valid date of the form DD/MM/YYYY")
+        if not isinstance(occupation, str):
+            raise TypeError("That is not a valid occupation.")
+        if not isinstance(bal, float):
+            raise TypeError("That is not a valid balance.")
+        if not isinstance(overlim, float):
+            raise TypeError("That is not a valid overdraft limit.")
 
-        self.name = name
+
+        self.name = fname
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 
 from tkinter import *
 from tkinter import ttk
-root = Tk()
-home = ttk.Frame(root, padding=10)
-home.grid()
-ttk.Label(home, text="Welcome to the banking app").grid(column=0, row=0)
-ttk.Button(home, text="Quit", command=root.destroy).grid(column=1, row=1)
-root.mainloop()
+import client
+
+def main():
+    None
+
+def start():
+    root = Tk()
+    root.title("Welcome")
+    home = ttk.Frame(root, padding=10)
+    home.grid()
+    ttk.Label(home, text="Welcome to the banking app").grid(column=1, row=0)
+    ttk.Button(home, text="Quit", command=root.destroy).grid(column=2, row=1)
+    ttk.Button(home, text="Start", command=main).grid(column=0,row=1)
+    root.mainloop()
